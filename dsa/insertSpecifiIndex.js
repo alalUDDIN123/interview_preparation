741d5b1f823abd20ad029a611c7df92dc1e328ad
alalUDDIN123 <alalu75666@gmail.com>
2023-06-08 19:19:11 +0530

    updated

--- dsa/insertSpecifiIndex.js
+++
@@ -1,8 +0,0 @@
-const fruits = ["apple", "banana", "orange"];
-fruits.splice(1,0,"grap");
-console.log(fruits); // Output: ["apple", "grape", "banana", "orange"]
-
-
-let arr = [1, 2, 3, 4, 5];
-arr.splice(3, 0, 5);
-console.log(arr); // Output: [1, 2, 3, 5, 4, 5]
